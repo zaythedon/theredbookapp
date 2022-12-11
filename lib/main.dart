3decf7d5616866cc7528cabe9921f0cb49aebd6a
anudeep-17 <anudeepsai88@gmail.com>
2022-12-10 21:29:41 -0500

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:theredbookapp/auth/login_page.dart';
 import 'package:theredbookapp/main_page.dart';
+import 'package:theredbookapp/screens/Archive.dart';
+import 'package:theredbookapp/screens/Redbook.dart';
 
 
 void main() {
--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:theredbookapp/screens/Archive.dart';
 import 'package:theredbookapp/screens/Redbook.dart';
 import 'package:theredbookapp/screens/Symposium.dart';
 import 'package:theredbookapp/util/RedBookDraw.dart';
@@ -36,7 +37,7 @@ class _HomepageState extends State<Homepage> {
                 padding: const EdgeInsets.all(8.0),
                 child: ClipRRect(
                   borderRadius: BorderRadius.all(Radius.circular(200)),
-                  child: Image.network("https://media-exp1.licdn.com/dms/image/C4E03AQGb9fj3h6Ladg/profile-displayphoto-shrink_200_200/0/1662221411927?e=1668038400&v=beta&t=2sgn-TcK6sFs3dfUMmG-jF548w8qnaRxiTug8ejGNn0",
+                  child: Image.network("https://avatars.githubusercontent.com/u/54953858?v=4",
                   ),
                 ),
               ),
@@ -94,7 +95,13 @@ class _HomepageState extends State<Homepage> {
                 label: ""
             ),
             BottomNavigationBarItem(
-                icon: current==3? Icon(Icons.person, color: Colors.redAccent,):Icon(Icons.person),
+                icon: current==3? GestureDetector(
+                  onTap:  () => {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => Archive()))},
+                  child: Icon(Icons.person, color: Colors.redAccent,),):GestureDetector(
+                  onTap:  () => {Archive()},
+                  child: Icon(Icons.person, color: Colors.black,),),
                 label: ""
             ),
           ],
