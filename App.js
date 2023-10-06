80c8f47788dfe28cdc78dff2ac34f2b56cf2f4c5
BJerrell-ASU <bjerrell@asu.edu>
2023-10-06 10:52:50 -0700

    Version Updates & Navigation Added
    Version of Expo, React, React-Native, and other dependencies Updated.
    
    Framework for navigation between screens added to Root App.js

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          title="Press me"
+          onPress={() => navigation.navigate('Details')}
+        />
+        <StatusBar style="auto" />
+      </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button
+          title="Click me"
+          onPress={() => navigation.navigate('Home')}
+        />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>{
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    }</NavigationContainer>
   );
 }
 
